fe2f8b308db85cc7bceefb837a10fb059ab716e7
aow1980 <wilkes.allen@gmail.com>
2015-04-28 21:42:44 -0400

    Inline Kernel Building Updates
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,6 +31,11 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02900000 --tags_offset 0x02700000
 
 TOUCH_BOOST_DEBUG := false
 
+# Define kernel config for inline building
+TARGET_KERNEL_CONFIG := hammerhead_defconfig
+TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
+TARGET_GCC_VERSION_ARM := 4.9
+
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
 # Increase the size if shaders of size greater than 12KB are used.
