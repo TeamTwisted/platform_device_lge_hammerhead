4c18c83a0d69b3b947bcfaf438d34869b07a24b2
aow1980 <wilkes.allen@gmail.com>
2015-08-25 20:02:56 -0400

    test settings

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ TOUCH_BOOST_DEBUG := false
 # Define kernel config for inline building
 TARGET_KERNEL_CONFIG := twisted_defconfig
 TARGET_KERNEL_SOURCE := kernel/lge/hammerhead
-TARGET_GCC_VERSION_ARM := 4.9
+TARGET_GCC_VERSION_ARM := 5.2
 
 # Shader cache config options
 # Maximum size of the  GLES Shaders that can be cached for reuse.
